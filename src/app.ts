d3af1446469cf3514a6ac557c1410525e03908da
ShonSagoro <ramosproque@gmail.com>
2024-07-21 03:45:52 -0600

    add gateway

--- src/app.ts
+++ src/app.ts
@@ -6,6 +6,7 @@ import proxy from "express-http-proxy";
 import JWTMiddleware from "./middleware/JWTMiddleware";
 import { AuthTokenBanned } from "./infraestructure/Dependencies";
 import { rateLimiter } from "./middleware/RateLimiter";
+import bodyParser from "body-parser";
 
 dotenv.config();
 
@@ -14,6 +15,9 @@ const signale = new Signale();
 const PORT = process.env.PORT || 3000;
 const GATEWAY = process.env.SERVICE_NAME;
 let server = null;
+
+app.use(bodyParser.json({ limit: '50mb' }));
+app.use(bodyParser.urlencoded({ extended: true, limit: '50mb' }));
 
 const user_microservices_url = 'https://onwork-user.integrador.xyz';
 const valoration_microservices_url = 'https://onwork-publication.integrador.xyz';
@@ -32,6 +36,7 @@ const publicPaths = [
   '/api/v1/users/health',
   '/api/v1/users/activate/[^/]+',
   '/api/v1/users/ubication/[^/]+', //todo: check this
+  '/api/v1/users/token/[^/]+', //todo: check this
   '/api/v1/gateway/health',
   '/api/v1/valorations/health',
   '/api/v1/chats/health',
@@ -86,20 +91,20 @@ app.use('/api/v1/valorations', proxy(valoration_microservices_url, {
   },
   }
 ));
+
 app.use('/api/v1/publications', proxy(valoration_microservices_url, {
   proxyReqPathResolver: (req) => {
     return `/publications${req.url.replace(/\/$/, '')}`;
   },
   }
 ));
+
 app.use('/api/v1/comments', proxy(valoration_microservices_url, {
   proxyReqPathResolver: (req) => {
     return `/comments${req.url.replace(/\/$/, '')}`;
   },
   }
 ));
-
-
 
 app.get('/api/v1/gateway/health', (req, res) => {
   res.status(200).json({ message: 'Gateway is healthy' });
